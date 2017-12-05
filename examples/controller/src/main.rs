73c98b7b03051b3887ee47489307059b1cd7dc36
Maid Dog <maiddogsrl@gmail.com>
2017-12-05 10:06:23 -0600

    Add version of project used in demo

--- examples/controller/src/main.rs
+++ examples/controller/src/main.rs
@@ -1,18 +1,48 @@
+extern crate pancurses;
 extern crate serialport;
 extern crate xbee;
 
 use xbee::*;
-use std::io::prelude::*;
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
 use serialport::SerialPortType;
 
+use pancurses::*;
+
+const COLOR_TABLE: [i16; 8] = [COLOR_RED,
+                                COLOR_BLUE,
+                                COLOR_GREEN,
+                                COLOR_CYAN,
+                                COLOR_RED,
+                                COLOR_MAGENTA,
+                                COLOR_YELLOW,
+                                COLOR_WHITE];
+
 fn main() {
-    let port_name = match select_port() {
+    let mut window = initscr();
+    let current = window.subwin(5, 28, 5, 41).expect("Could not make subwindow");
+
+    let title = "Sparkies Demo";
+    
+    window.nodelay(true);
+    noecho();
+
+    curs_set(0);
+    window.keypad(true);
+
+    for (i, color) in COLOR_TABLE.into_iter().enumerate() {
+        init_pair(i as i16, *color, COLOR_BLACK);
+    }
+
+    window.clear();
+    window.mvprintw(0, (window.get_max_x() / 2) - ((title.len() / 2) as i32), title);
+    window.refresh();
+
+    let port_name = match select_port(&mut window) {
         Some(name) => name,
         None => {
-            println!("Could not select port. Quitting.");
+            window.mvprintw(window.get_max_y() - 1, 0, "Could not select port. Quitting.");
             return;
         }
     };
@@ -22,139 +52,151 @@ fn main() {
         Err(why) => {
             match why {
                 xbee::Error::SerialError(err) => {
-                    println!("Serial error: {}", err.description());
+                    window.mvprintw(window.get_max_y() - 1, 0, &format!("Serial error: {}", err.description()));
                 }
-                _ => println!("Other"),
+                _ => {
+                    window.mvprintw(window.get_max_y() - 1, 0, "Other error.");
+                    ()
+                }
             }
             return
         }
     };
 
+    window.mv(1, 0);
+    window.clrtobot();
+
+    window.mvprintw(5, 10, "Available commands:");
+    window.mvprintw(6, 10, "1. Set threshold");
+    window.mvprintw(7, 10, "2. Exit\n");
+    window.refresh();
+
     loop {
-        println!("\nChoose a command:");
-        println!("1. Set threshold");
-        println!("2. Get data");
-        println!("3. Exit\n");
+        match window.getch() {
+            Some(Input::Character('1')) => {
+                let mut value = String::new();
 
-        let mut input = String::new();
-        let stdin = std::io::stdin();
+                loop {
+                    window.mvprintw(window.get_max_y() - 1, 0, "Enter new threshold: ");
+                    window.clrtobot();
 
-        stdin.lock()
-            .read_line(&mut input)
-            .expect("Could not read line");
+                    echo();
+                    loop {
+                        if let Some(Input::Character(c)) = window.getch() {
+                            if c == '\n' {
+                                break;
+                            }
+                            value.push(c);
+                        }
+                    }
+                    noecho();
 
-        input = input.trim().into();
+                    value = value.trim().into();
 
-        if input == "1" {
-            let mut value = String::new();
+                    if let Ok(_) = value.parse::<u32>() {
+                        break;
+                    } else {
+                        value.clear();
+                    }
+                }
 
-            loop {
-                print!("Enter new threshold: ");
-                let _ = std::io::stdout().flush();
+                let mut tries = 0;
 
-                stdin.lock()
-                    .read_line(&mut value)
-                    .expect("Could not read line");
+                loop {
+                    if tries >= 10 {
+                        window.mvprintw(window.get_max_y() - 1, 0, "Could not set after 10 tries.");
+                        break;
+                    }
 
-                value = value.trim().into();
+                    if let Err(why) = xbee.write_raw(format!("set {}", value)) {
+                        window.mvprintw(window.get_max_y() - 1, 0, &format!("Could not send set request: {:?}", why));
+                    }
 
-                if let Ok(_) = value.parse::<u32>() {
-                    break;
+                    let mut resp = xbee.read_raw();
+                    resp = resp.trim().into();
+                    
+                    if resp == "OK" {
+                        window.mvprintw(window.get_max_y() - 1, 0, &format!("New threshold set to {}", value));
+                        break;
+                    } else {
+                        tries += 1;
+                    }
+
+                    thread::sleep(Duration::from_millis(100));
                 }
             }
-
-            let mut tries = 0;
-
-            loop {
-                if tries >= 10 {
-                    println!("Could not set after 10 tries.");
-                    break;
-                }
-
-                if let Err(why) = xbee.write_raw(format!("set {}", value)) {
-                    println!("Could not send set request: {:?}", why);
-                }
-
-                let mut resp = xbee.read_raw();
-                resp = resp.trim().into();
-                
-                if resp == "OK" {
-                    println!("New threshold set to {}", value);
-                    break;
-                } else {
-                    tries += 1;
-                }
-
-                thread::sleep(Duration::from_millis(100));
+            Some(Input::Character('2')) => {
+                break;
             }
-        } 
-        else if input == "2" {
-            let mut tries = 0;
-            loop {
-                if tries >= 10 {
-                    println!("Could not get data after 10 tries.");
-                    break;
-                }
+            _ => (),
+        }
 
-                if let Err(why) = xbee.write_raw("get") {
-                    println!("Could not send get request: {:?}", why);
-                    continue;
-                }
+        if let Err(why) = xbee.write_raw("get") {
+            println!("Could not send get request: {:?}", why);
+            continue;
+        }
 
-                let resp = xbee.read_raw();
+        let resp = xbee.read_raw();
 
-                let values = resp
-                    .split_whitespace()
-                    .filter_map(|v| v.parse::<f64>().ok())
-                    .collect::<Vec<f64>>();
+        let values = resp
+            .split_whitespace()
+            .filter_map(|v| v.parse::<f64>().ok())
+            .collect::<Vec<f64>>();
 
-                if values.len() < 3 {
-                    tries += 1;
-                } else {
-                    println!("Temperature: {}°C", values[0]);
-                    println!("Humidity   : {}%", values[1]);
-                    println!("Threshold : {}", values[2]);
-                    break;
-                }
-                thread::sleep(Duration::from_millis(100));
-            }
-        } else if input == "3" {
-            break;
-        } else {
-            println!("Invalid selection: {}", input);
+        if values.len() == 3 {
+            let title = "Current data";
+            current.clear();
+            current.draw_box(0, 0);
+            current.mvprintw(0, (current.get_max_x() / 2) - ((title.len() / 2) as i32), title);
+            current.mvprintw(1, 1, &format!("Temperature: {} C", values[0]));
+            current.mvprintw(2, 1, &format!("Humidity   : {}%", values[1]));
+            current.mvprintw(3, 1, &format!("Threshold  : {}", values[2]));
+            current.refresh();
         }
     }
 }
 
-fn select_port() -> Option<String> {
+fn select_port(window: &mut Window) -> Option<String> {
     let ports = match serialport::available_ports() {
         Ok(ports) => ports,
         Err(why) => {
-            println!("Could not enumerate serial ports: {:?}", why);
+            window.mvprintw(window.get_max_y() - 1, 0, &format!("Could not enumerate serial ports: {:?}", why));
+            window.refresh();
             return None;
         }
     };
 
     if ports.len() == 0 {
-        println!("No serial ports found.");
+        window.mvprintw(window.get_max_y() - 1, 0, "No serial ports found.");
+        window.refresh();
         return None;
     }
 
+    let prompt = "Select a port to connect to:";
+
     loop {
-        println!("Select a port to connect to:");
+        window.mvprintw(1, (window.get_max_x() / 2) - ((prompt.len() / 2) as i32), prompt);
 
         for (index, port) in ports.iter().enumerate() {
             if let SerialPortType::UsbPort(ref info) = port.port_type {
-                println!("{}: {} - {}", index + 1, port.port_name, info.product.as_ref().map_or("", String::as_str));
+                window.mvprintw(index as i32 + 2, 10, &format!("{}: {} - {}", index + 1, port.port_name, info.product.as_ref().map_or("", String::as_str)));
             }
         }
+        window.refresh();
 
-        let stdin = std::io::stdin();
         let mut input = String::new();
+        loop {
+            match window.getch() {
+                Some(Input::Character(c)) => {
+                    if c == '\n' {
+                        break;
+                    }
 
-        stdin.lock()
-            .read_line(&mut input)
-            .expect("Could not read from stdin.");
+                    input.push(c);
+                }
+                _ => (),
+            }
+        }
 
         let trimmed = input.trim();
 
@@ -162,7 +204,10 @@ fn select_port() -> Option<String> {
             Ok(num) if num > 0 && num <= ports.len() => {
                 return Some(ports.get(num - 1).unwrap().port_name.clone())
             }
-            _ => println!("Invalid selection."),
+            _ => {
+                window.mvprintw(window.get_max_y() - 1, 0, "Invalid selection.");
+                ()
+            }
         }
     }
 }
